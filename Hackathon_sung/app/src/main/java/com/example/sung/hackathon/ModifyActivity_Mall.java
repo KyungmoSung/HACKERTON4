4c1f6e361ba24f75199e2aef9cb3c74086b955db
Minwoo <alsdnv112@naver.com>
2016-08-21 13:37:55 +0900

    Crolling

--- Hackathon_sung/app/src/main/java/com/example/sung/hackathon/ModifyActivity_Mall.java
+++ Hackathon_sung/app/src/main/java/com/example/sung/hackathon/ModifyActivity_Mall.java
@@ -1,16 +1,105 @@
 package com.example.sung.hackathon;
 
 import android.app.Activity;
+import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.sung.hackathon.mall.Croll;
+import com.example.sung.hackathon.mall.Mall;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Locale;
 
 /**
  * Created by 희연 on 2016-08-18.
  */
 public class ModifyActivity_Mall extends Activity {
 
+
+    static final int NMALL = 4;
+    Button bt[] = new Button[NMALL];
+    static Mall mall[] = new Mall[NMALL] ;//= new Mall[NMALL];
+    private static Croll croll;
+
+
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.modify_mall);
+        mall[0] = new Mall();
+        mall[1] = new Mall();
+        mall[2] = new Mall();
+        mall[3] = new Mall();
+        bt[0] = (Button)findViewById(R.id.button1);
+        for(Mall i : mall){
+            if(i.isSync()) {
+                bt[0].setText("동기중");
 
+            }
+            else{
+
+
+                bt[0].setOnClickListener(listener(bt[0]));
+            }
+
+        }
+
+    }
+    private static View.OnClickListener listener(View v){
+
+        View.OnClickListener my_listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LayoutInflater dialog = LayoutInflater.from(v.getContext());
+                final View dialogLayout = dialog.inflate(R.layout.mall_login_dialog, null);
+                final Dialog addDialog = new Dialog(v.getContext());
+
+                addDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+                addDialog.setContentView(dialogLayout);
+                addDialog.show();
+                Button login = (Button)dialogLayout.findViewById(R.id.login);
+                Button cancel = (Button)dialogLayout.findViewById(R.id.cancel);
+                login.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        switch(v.getId()){
+                            case R.id.button1:
+                                mall[0].setId(((EditText)dialogLayout.findViewById(R.id.input_id)).getText().toString());
+                                mall[0].setPw(((EditText)dialogLayout.findViewById(R.id.input_pw)).getText().toString());
+                                break;
+                            case R.id.button2:
+                                break;
+                            case R.id.button3:
+                                break;
+                            case R.id.button4:
+                                break;
+                        }
+                        croll = new Croll();
+                        croll.Login();
+
+                        addDialog.cancel();
+                    }
+                });
+                cancel.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        addDialog.cancel();
+                    }
+                });
+            }
+        };
+
+
+
+
+        return my_listener;
     }
 }
---
+++ Hackathon_sung/app/src/main/java/com/example/sung/hackathon/mall/Croll.java
@@ -0,0 +1,91 @@
+package com.example.sung.hackathon.mall;
+
+import com.gargoylesoftware.htmlunit.BrowserVersion;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlAnchor;
+import com.gargoylesoftware.htmlunit.html.HtmlButton;
+import com.gargoylesoftware.htmlunit.html.HtmlForm;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.gargoylesoftware.htmlunit.html.HtmlPasswordInput;
+import com.gargoylesoftware.htmlunit.html.HtmlTable;
+import com.gargoylesoftware.htmlunit.html.HtmlTableRow;
+import com.gargoylesoftware.htmlunit.html.HtmlTextInput;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+/**
+ * Created by Minwoo on 2016. 8. 19..
+ */
+public class Croll extends Mall{
+
+    public static Map<String, String> cookies;
+    private String id = "mw9027";
+    private String pw = "jy2411";
+    WebClient webClient;
+    HtmlPage currPage;
+    boolean login_success;
+    String LoginPage = "http://member.ssg.com/member/popup/popupLogin.ssg";
+    String OrderPage = "http://pay.ssg.com/myssg/orderList.ssg?gnb=orderlist";
+
+    public Croll() {
+
+
+    }
+
+    public ArrayList<String> getData(){
+
+        ArrayList<String> food = new ArrayList<String>();
+
+        try {
+
+            if(!login_success)
+                Login();
+
+            currPage = webClient.getPage(OrderPage);
+            HtmlTable table = (HtmlTable) currPage.getElementById("table_order_list");
+
+            for (HtmlTableRow span : table.getBodies().get(0).getRows()) {
+
+                HtmlAnchor anchor = (HtmlAnchor) span.getElementsByTagName("a").get(0);
+                HtmlTable table2 ;
+                currPage = anchor.click();
+                webClient.waitForBackgroundJavaScript(30000);
+                table2 = (HtmlTable) currPage.getElementById("eachCart0");
+                for (HtmlTableRow span2 : table2.getBodies().get(0).getRows()) {
+                    food.add(span2.getElementsByTagName("a").get(0).getTextContent());
+                }
+
+
+            }
+        } catch (Exception e) {// TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return food;
+    }
+    public boolean Login(){
+
+        try{
+            webClient = new WebClient(BrowserVersion.FIREFOX_38);
+            currPage = webClient.getPage(LoginPage);
+            HtmlForm form = (HtmlForm) currPage.getForms().get(0);
+            HtmlTextInput inputId = form.getInputByName("mbrLoginId");
+            HtmlPasswordInput inputPw = (HtmlPasswordInput) form.getInputByName("password");
+            HtmlButton button = (HtmlButton) form.getElementsByTagName("button").get(2);
+            inputId.setValueAttribute(id);
+            inputPw.setValueAttribute(pw);
+            button.click();
+            webClient.waitForBackgroundJavaScript(30000);
+
+            login_success = true;
+
+        }catch (Exception e){
+
+
+            e.printStackTrace();
+            login_success = false;
+        }
+        return login_success;
+    }
+
+}
